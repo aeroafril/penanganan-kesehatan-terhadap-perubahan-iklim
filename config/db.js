e5b08244c952ddf02a423a520f3caccae6279ab7
Aero Afril <semarg53@gmail.com>
2026-07-26 12:12:05 +0700

    Add connection caching to MongoDB connection function
    Implement connection caching for MongoDB to improve performance.

--- config/db.js
+++ config/db.js
@@ -1,12 +1,24 @@
 const mongoose = require('mongoose');
 
+let cachedConnection = null;
+
 async function connectDB() {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log('MongoDB terhubung');
-    } catch (err) {
-        console.error('Gagal konek MongoDB:', err.message);
+    if (cachedConnection) {
+        return cachedConnection;
     }
+
+    cachedConnection = mongoose.connect(process.env.MONGODB_URI)
+        .then((conn) => {
+            console.log('MongoDB terhubung');
+            return conn;
+        })
+        .catch((err) => {
+            console.error('Gagal konek MongoDB:', err.message);
+            cachedConnection = null;
+            throw err;
+        });
+
+    return cachedConnection;
 }
 
 module.exports = connectDB;
